80ee1bda1a59ca2de50cbba41c8702c88031a62e
Pranav <pranav.shukla1282@gmail.com>
2022-10-08 17:24:23 +0530

    setup devise with login/signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root "home#index"
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # devise_scope :user do
+  #   # Redirests signing out users back to sign-in
+  #   get "/users", to: "devise/sessions#new"
+  # end
 end
